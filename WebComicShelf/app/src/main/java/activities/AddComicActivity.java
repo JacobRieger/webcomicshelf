89a0c199d62246af48dd4612045a28b9192e5f38
Jacob Rieger <jacob@talentriot.com>
2014-04-23 18:05:41 -0600

    Added all private variables

--- WebComicShelf/app/src/main/java/activities/AddComicActivity.java
+++ WebComicShelf/app/src/main/java/activities/AddComicActivity.java
@@ -3,20 +3,45 @@ package activities;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.app.R;
 
+import services.utilities.BookmarkList;
+import services.utilities.BookmarkLoader;
+
 public class AddComicActivity extends ActionBarActivity {
+
+
+    private WebView webview;
+    private TextView name;
+    private EditText edittext;
+    private BookmarkList Bookmarks;
+    private Button addComicButton;
+    private String         comicName;
+    private String         comicImageUrl = "Notset";
+    private String         comicWebsite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_comic);
+
+        if (Bookmarks == null) {
+
+            Bookmarks = new BookmarkLoader(this).getBookmarks();
+        }
+
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -28,9 +53,26 @@ public class AddComicActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.add_comic, menu);
+
+        //This generates our bookmark submenu items
+        MenuItem BookMark = (MenuItem) menu.findItem(R.id.addComicBookmarks);
+
+        SubMenu BookMarkSub = null;
+        if (BookMark != null) {
+            BookMarkSub = BookMark.getSubMenu();
+        }
+        else
+        {
+            Log.d("AddComicActivity", "Bookmark submenu null");
+            return false;
+        }
+
+        for(int x = 0; x < Bookmarks.size(); x++)
+        {
+            BookMarkSub.add(32, 0, 0, Bookmarks.get(x).getName());
+        }
+
         return true;
     }
 
@@ -39,10 +81,7 @@ public class AddComicActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
         return super.onOptionsItemSelected(item);
 
     }
